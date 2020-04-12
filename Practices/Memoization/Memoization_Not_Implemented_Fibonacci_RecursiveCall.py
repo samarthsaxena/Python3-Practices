4a1f8eb02168cd151dd6cf43cc42e7aab926cec9
samarthsaxena <samarth.saxena@outlook.com>
2020-04-12 18:18:10 +0530

    Added type chaecking in formal args and return type

--- Practices/Memoization/Memoization_Not_Implemented_Fibonacci_RecursiveCall.py
+++ Practices/Memoization/Memoization_Not_Implemented_Fibonacci_RecursiveCall.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-#Memoization not implemented
+# Memoization not implemented
 
-def fibonacci(number):
-
+def fibonacci(number: int) -> int:
     if number == 1:
         return 1
     elif number == 2:
         return 1
     elif number > 2:
-        return fibonacci(number - 1) + fibonacci(number -2)
-
-for i in range(1,101):
-    print(i," : ", fibonacci(i))
+        return fibonacci(number - 1) + fibonacci(number - 2)
 
 
-#A very serious performance issue exists her without Memoization.
+for i in range(1, 101):
+    print(i, " : ", fibonacci(i))
+
+# A very serious performance issue exists here without Memoization.
