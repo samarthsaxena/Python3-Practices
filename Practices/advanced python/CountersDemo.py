c91ffa6ac4360a4099a0f762f7f91a4d3425cd22
samarthsaxena <samarth.saxena@outlook.com>
2020-04-17 01:23:36 +0530

    function as an example for defaultdict which can have a custome value.

--- Practices/advanced python/CountersDemo.py
+++ Practices/advanced python/CountersDemo.py
@@ -2,6 +2,8 @@
 # https://book.pythontips.com/en/latest/collections.html#counter
 
 from collections import Counter as cnt
+
+
 def main():
     # list of students in class 1
     class1 = ["Bob", "Becky", "chad", "Darcy", "frank", "Hannah", "kevin", "james", "malanie", "panny", "steve"]
@@ -19,18 +21,18 @@ def main():
           f'Is that Correct? {"Yes" if sum(c1.values()) == len(class1) else "No"}')
     # TODO:  Combine two classes (This just to know whats the sum of all objects in totality )
     c1.update(class2)
-    print(f'Total students (class1 + class2) :{sum(c1.values()) }')
+    print(f'Total students (class1 + class2) :{sum(c1.values())}')
 
-
-    #TODO: most common name in class1 and class2
+    # TODO: most common name in class1 and class2
     print(f'So after combining the class1 and class 2. the top 3 most common names are {c1.most_common(3)}')
 
-    #TODO : seperate the classes again
+    # TODO : seperate the classes again
     c1.subtract(class2)
     print(f'After the subtraction class 1\'s top 3 most common names are {c1.most_common(3)}')
 
     # TODO: What's most common between these 2 classes
     print(f'{c1 & c2}')
+
 
 if __name__ == '__main__':
     main()
