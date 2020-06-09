64d588ad2bdbecdb2bcd026784b79aefe2338127
samarthsaxena <samarth.saxena@outlook.com>
2020-06-10 02:14:40 +0530

    Applied json property load for app config data.

--- flaskwebapp/flaskblog/config.py
+++ flaskwebapp/flaskblog/config.py
@@ -1,12 +1,23 @@
+import json
 import os
+
+
+def file_path() -> str:
+    filepath = os.path.abspath('flaskblog/config_vars/config.json')
+    print(f'Found file path for config.json: {filepath}')
+    return filepath
+
+
+with open(file_path()) as config_file:
+    config = json.load(config_file)
 
 
 class Config:
     # Add a secret key to Application, in order to protect against Xss and hacks
-    # Generated from python3 secrets module with token_hex(soem int value)
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # Generated from python3 secrets module with token_hex(some int value)
+    SECRET_KEY = config.get('SECRET_KEY')
     # To work wit DB, we'd use sqlite db
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
 
     # SMTP Configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -15,7 +26,5 @@ class Config:
     # the gmail account's username and password is better being hidden
     # The required data is good to have in environment variables or some encrypted config file on server.
     # for now let's just use the environment variables
-    MAIL_USERNAME = os.environ.get('TEST_EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('TEST_EMAIL_PASS')
-
-
+    MAIL_USERNAME = config.get('TEST_EMAIL_USER')
+    MAIL_PASSWORD = config.get('TEST_EMAIL_PASS')
